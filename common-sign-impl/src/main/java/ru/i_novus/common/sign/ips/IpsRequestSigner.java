27284c460d74e31f5e617e3b88d7be6748643df6
igilfanov <igilfanov@i-novus.ru>
2019-01-16 18:43:35 +0300

    rf небольшой рефакторинг

--- common-sign-impl/src/main/java/ru/i_novus/common/sign/ips/IpsRequestSigner.java
+++ common-sign-impl/src/main/java/ru/i_novus/common/sign/ips/IpsRequestSigner.java
@@ -1,19 +1,16 @@
 package ru.i_novus.common.sign.ips;
 
-import org.apache.xml.security.c14n.CanonicalizationException;
-import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Node;
 import ru.i_novus.common.sign.GostXmlSignature;
 import ru.i_novus.common.sign.api.SignAlgorithmType;
+import ru.i_novus.common.sign.exception.CommonSignFailureException;
 import ru.i_novus.common.sign.util.CryptoFormatConverter;
+import ru.i_novus.common.sign.util.XPathUtil;
 
 import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.*;
 import javax.xml.transform.TransformerException;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 import java.util.UUID;
@@ -34,16 +31,16 @@ public final class IpsRequestSigner {
     /**
      * Подписывает SOAP-запрос для сервиса ИПС
      *
-     * @param message сообщение
-     * @param soapService адрес сервиса в ИПС
-     * @param soapAction действие сервиса
-     * @param clientEntityId идентификатор системы
+     * @param message            сообщение
+     * @param soapService        адрес сервиса в ИПС
+     * @param soapAction         действие сервиса
+     * @param clientEntityId     идентификатор системы
      * @param encodedCertificate сертификат в формате PEM
-     * @param encodedPrivateKey закрытый ключ в формате PEM
+     * @param encodedPrivateKey  закрытый ключ в формате PEM
+     * @throws CommonSignFailureException
      */
     public static void signIpsRequest(SOAPMessage message, String soapService, String soapAction, String clientEntityId,
-                                      String encodedCertificate, String encodedPrivateKey) throws
-            SOAPException, GeneralSecurityException, TransformerException, InvalidCanonicalizerException, CanonicalizationException, IOException {
+                                      String encodedCertificate, String encodedPrivateKey) throws CommonSignFailureException {
 
         CryptoFormatConverter converter = CryptoFormatConverter.getInstance();
         X509Certificate x509Certificate = converter.getCertificateFromPEMEncoded(encodedCertificate);
@@ -54,45 +51,40 @@ public final class IpsRequestSigner {
     /**
      * Подписывает SOAP-запрос для сервиса ИПС
      *
-     * @param message сообщение
-     * @param soapService адрес сервиса в ИПС
-     * @param soapAction действие сервиса
+     * @param message        сообщение
+     * @param soapService    адрес сервиса в ИПС
+     * @param soapAction     действие сервиса
      * @param clientEntityId идентификатор системы
-     * @param certificate сертификат в формате
-     * @param privateKey закрытый ключ в формате {@link java.security.PrivateKey}
+     * @param certificate    сертификат в формате
+     * @param privateKey     закрытый ключ в формате {@link java.security.PrivateKey}
+     * @throws CommonSignFailureException
      */
-    public static void signIpsRequest(SOAPMessage message, String soapService, String soapAction, String clientEntityId,
-                                      PrivateKey privateKey, X509Certificate certificate) throws
-            SOAPException, GeneralSecurityException, TransformerException, InvalidCanonicalizerException, CanonicalizationException, IOException {
-        // Добавляем требуемые пространства имен
-        message.getSOAPPart().getEnvelope().addNamespaceDeclaration("wsse", WSSE_NS)
-                .addNamespaceDeclaration("wsu", WSU_NS)
-                .addNamespaceDeclaration("ds", DS_NS)
-                .addNamespaceDeclaration("wsa", WSA_NS);
-        // Проставляем идентификатор для элемента Body
-        message.getSOAPBody().setAttribute("wsu:Id", "body");
-        // Добавляем элементы transportHeader, authInfo и clientEntityId
-        Node transportHeader = XPathAPI.selectSingleNode(message.getSOAPHeader(),
-                "//*[local-name()='transportHeader']/*[local-name()='authInfo']/*[local-name()='clientEntityId']");
-        if (transportHeader == null) {
-            message.getSOAPHeader().addChildElement(new QName(EGISZ_NS, "transportHeader", EGISZ_PREFIX))
-                    .addChildElement("authInfo", EGISZ_PREFIX)
-                    .addChildElement("clientEntityId", EGISZ_PREFIX)
-                    .addTextNode(clientEntityId);
+    public static void signIpsRequest(SOAPMessage message, String soapService, String soapAction, String clientEntityId, PrivateKey privateKey, X509Certificate certificate) throws CommonSignFailureException {
+
+        try {
+
+            // Добавляем требуемые пространства имен
+            addNamespaceDeclaration(message.getSOAPPart().getEnvelope());
+
+            // Проставляем идентификатор для элемента Body
+            setBodyIdAttribute(message.getSOAPBody());
+
+            SOAPHeader soapHeader = message.getSOAPHeader();
+
+            // Добавляем элементы transportHeader, authInfo и clientEntityId
+            addTransportHeaderElem(soapHeader, clientEntityId);
+
+            // Добавляем элементы MessageID, Action и To
+            addMessageIdElem(soapHeader);
+
+            addActionElem(soapHeader, soapAction);
+
+            addToElem(message, soapService);
+
+        } catch (SOAPException | TransformerException | RuntimeException e) {
+            throw new CommonSignFailureException(e);
         }
-        // Добавляем элементы MessageID, Action и To
-        Node messageId = XPathAPI.selectSingleNode(message.getSOAPHeader(), "//*[local-name()='MessageID']");
-        if (messageId == null) {
-            message.getSOAPHeader().addChildElement("MessageID", "wsa").addTextNode(UUID.randomUUID().toString());
-        }
-        Node action = XPathAPI.selectSingleNode(message.getSOAPHeader(), "//*[local-name()='Action']");
-        if (action == null) {
-            message.getSOAPHeader().addChildElement("Action", "wsa").addTextNode(soapAction);
-        }
-        Node to = XPathAPI.selectSingleNode(message.getSOAPHeader(), "//*[local-name()='To']");
-        if (to == null) {
-            message.getSOAPHeader().addChildElement("To", "wsa").addTextNode(soapService);
-        }
+
         SignAlgorithmType signAlgorithmType = SignAlgorithmType.findByAlgorithmName(certificate.getSigAlgName());
         // Добавляем элемент Security
         GostXmlSignature.addSecurityElement(message, certificate, null);
@@ -103,12 +95,12 @@ public final class IpsRequestSigner {
     /**
      * Подписывает SOAP-ответ для сервиса ИПС
      *
-     * @param message сообщение
+     * @param message            сообщение
      * @param encodedCertificate сертификат в формате PEM
-     * @param encodedKey закрытый ключ в формате PEM
+     * @param encodedKey         закрытый ключ в формате PEM
+     * @throws CommonSignFailureException
      */
-    public static void signIpsResponse(SOAPMessage message, String encodedCertificate, String encodedKey) throws SOAPException,
-            TransformerException, GeneralSecurityException, InvalidCanonicalizerException, CanonicalizationException, IOException {
+    public static void signIpsResponse(SOAPMessage message, String encodedCertificate, String encodedKey) throws CommonSignFailureException {
         CryptoFormatConverter converter = CryptoFormatConverter.getInstance();
         X509Certificate certificate = converter.getCertificateFromPEMEncoded(encodedCertificate);
         signIpsResponse(message, converter.getPKFromPEMEncoded(SignAlgorithmType.findByCertificate(certificate), encodedKey), certificate);
@@ -116,27 +108,72 @@ public final class IpsRequestSigner {
 
     /**
      * Подписывает SOAP-ответ для сервиса ИПС
-     * @param message сообщение
-     * @param privateKey закрытый ключ в формате PEM
+     *
+     * @param message     сообщение
+     * @param privateKey  закрытый ключ в формате PEM
      * @param certificate сертификат в формате PEM
+     * @throws CommonSignFailureException
      */
-    public static void signIpsResponse(SOAPMessage message, PrivateKey privateKey, X509Certificate certificate) throws SOAPException,
-            TransformerException, GeneralSecurityException, InvalidCanonicalizerException, CanonicalizationException, IOException {
-        // Добавляем требуемые пространства имен
-        message.getSOAPPart().getEnvelope().addNamespaceDeclaration("wsse", WSSE_NS)
-                .addNamespaceDeclaration("wsu", WSU_NS)
-                .addNamespaceDeclaration("ds", DS_NS)
-                .addNamespaceDeclaration("wsa", WSA_NS);
-        // Проставляем идентификатор для элемента Body
-        message.getSOAPBody().setAttribute("wsu:Id", "body");
-        // Добавляем элементы MessageID
-        Node messageId = XPathAPI.selectSingleNode(message.getSOAPHeader(), "//*[local-name()='MessageID']");
-        if (messageId == null) {
-            message.getSOAPHeader().addChildElement("MessageID", "wsa").addTextNode(UUID.randomUUID().toString());
+    public static void signIpsResponse(SOAPMessage message, PrivateKey privateKey, X509Certificate certificate) throws CommonSignFailureException {
+
+        try {
+            // Добавляем требуемые пространства имен
+            addNamespaceDeclaration(message.getSOAPPart().getEnvelope());
+
+            // Проставляем идентификатор для элемента Body
+            setBodyIdAttribute(message.getSOAPBody());
+
+            // Добавляем элемент MessageID
+            addMessageIdElem(message.getSOAPHeader());
+
+        } catch (SOAPException | TransformerException | RuntimeException e) {
+            throw new CommonSignFailureException(e);
         }
+
         // Добавляем элемент Security
         GostXmlSignature.addSecurityElement(message, certificate, null);
         // Подписываем сообщение
         GostXmlSignature.sign(message, privateKey, SignAlgorithmType.findByCertificate(certificate));
+    }
+
+    private static void addMessageIdElem(SOAPHeader soapHeader) throws TransformerException, SOAPException {
+        Node messageId = XPathAPI.selectSingleNode(soapHeader, "//*[local-name()='MessageID']");
+        if (messageId == null)
+            soapHeader.addChildElement("MessageID", "wsa").addTextNode(UUID.randomUUID().toString());
+    }
+
+    private static void addTransportHeaderElem(SOAPHeader soapHeader, final String clientEntityId) throws TransformerException, SOAPException {
+
+        Node transportHeader = XPathUtil.selectSingleNode(soapHeader,
+                "//*[local-name()='transportHeader']/*[local-name()='authInfo']/*[local-name()='clientEntityId']");
+
+        if (transportHeader == null)
+            soapHeader.addChildElement(new QName(EGISZ_NS, "transportHeader", EGISZ_PREFIX))
+                    .addChildElement("authInfo", EGISZ_PREFIX)
+                    .addChildElement("clientEntityId", EGISZ_PREFIX)
+                    .addTextNode(clientEntityId);
+    }
+
+    private static void addActionElem(SOAPHeader soapHeader, final String soapAction) throws TransformerException, SOAPException {
+        Node action = XPathUtil.selectSingleNode(soapHeader, "//*[local-name()='Action']");
+        if (action == null)
+            soapHeader.addChildElement("Action", "wsa").addTextNode(soapAction);
+    }
+
+    private static void addToElem(SOAPMessage message, String soapService) throws SOAPException {
+        Node to = XPathUtil.selectSingleNode(message.getSOAPHeader(), "//*[local-name()='To']");
+        if (to == null)
+            message.getSOAPHeader().addChildElement("To", "wsa").addTextNode(soapService);
+    }
+
+    private static void setBodyIdAttribute(SOAPBody soapBody){
+        soapBody.setAttribute("wsu:Id", "body");
+    }
+
+    private static void addNamespaceDeclaration(SOAPEnvelope soapEnvelope) throws SOAPException {
+        soapEnvelope.addNamespaceDeclaration("wsse", WSSE_NS)
+                .addNamespaceDeclaration("wsu", WSU_NS)
+                .addNamespaceDeclaration("ds", DS_NS)
+                .addNamespaceDeclaration("wsa", WSA_NS);
     }
 }
