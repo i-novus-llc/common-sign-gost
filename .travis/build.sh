e2d473dce034aab0f0cf614067fd8aaf2bf4411e
Alexander Belov <abelov@i-novus.ru>
2020-08-31 13:05:44 +0300

    cf Change build settings.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,5 +3,7 @@
 if [[ $TRAVIS_TAG == '' ]] ; then
   mvn --settings $TRAVIS_DIR/.maven.xml clean install -B -U
 else
+  openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $TRAVIS_DIR/pubring.gpg.enc -out $TRAVIS_DIR/pubring.gpg -d
+  openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $TRAVIS_DIR/secring.gpg.enc -out $TRAVIS_DIR/secring.gpg -d
   mvn --settings $TRAVIS_DIR/.maven.xml clean deploy $TRAVIS_DIR/settings.xml -Prelease -B -U
 fi
