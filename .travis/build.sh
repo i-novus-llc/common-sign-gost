81f90d13aa576d7b3c00bb79c95d036baed6cea8
Alexander Belov <abelov@i-novus.ru>
2020-08-31 11:26:44 +0300

    cf Change build settings.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_TAG == '' ]] ; then
-  mvn --settings .travis/.maven.xml clean install -B -U
+  mvn --settings $TRAVIS_DIR/.maven.xml clean install -B -U
 else
-  openssl aes-256-cbc -K $encrypted_286124008a61_key -iv $encrypted_286124008a61_iv -in .travis/.my.travis.gpg.enc -out .travis/.my.travis.gpg -d
-  mvn --settings .travis/.maven.xml clean deploy -Prelease -B -U
+  mvn --settings $TRAVIS_DIR/.maven.xml clean deploy $TRAVIS_DIR/settings.xml -Prelease -B -U
 fi
