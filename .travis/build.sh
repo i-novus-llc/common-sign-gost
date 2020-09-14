2d0b2bcbcce8d10bb6903cab6bdbe8d743fbc476
Alexander Belov <abelov@i-novus.ru>
2020-09-14 17:02:59 +0300

    cf Change build settings.

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,5 +5,5 @@ if [[ $TRAVIS_TAG == '' ]] ; then
 else
   openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in .travis/codesigning.asc.enc -out .travis/signingkey.asc -d
   gpg --fast-import .travis/signingkey.asc
-  mvn --settings .travis/.maven.xml clean deploy -Prelease -B -U
+  mvn --settings .travis/.maven.xml clean deploy -DskipTests=true -Prelease -B -U
 fi
